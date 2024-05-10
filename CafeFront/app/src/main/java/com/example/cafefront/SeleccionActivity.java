0ba5cb5b2567b67c5ad6cc6e4ba6904f6fe9aec1
Cynthia Tieso Guimaraes <161252427+NotSureAbout15@users.noreply.github.com>
2024-05-11 01:01:48 +0200

    Creada primera pantalla de seleccion, añadidos botones y probada su funcionalidad

---
+++ CafeFront/app/src/main/java/com/example/cafefront/SeleccionActivity.java
@@ -0,0 +1,43 @@
+package com.example.cafefront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SeleccionActivity extends AppCompatActivity {
+    private Context context;
+    private Button trabajador;
+    private Button cliente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion);
+
+        context = this;
+        trabajador = findViewById(R.id.boton_trabajador);
+        cliente = findViewById(R.id.boton_cliente);
+
+        //al clicar en el boton trabajador, me mostrara un mensaje
+        trabajador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Has clicado en trabajador", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //al clicar en el boton cliente, me mostrará un mensaje
+        cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Has clicado en cliente", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
