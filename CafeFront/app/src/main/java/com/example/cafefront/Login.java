70a3676aba85e45955883fb2191a5c6c98642859
Cynthia Tieso Guimaraes <161252427+NotSureAbout15@users.noreply.github.com>
2024-05-11 02:18:10 +0200

    Creado formulario primario de login

---
+++ CafeFront/app/src/main/java/com/example/cafefront/Login.java
@@ -0,0 +1,40 @@
+package com.example.cafefront;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+    private Context context;
+    private Button iniciar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        context = this;
+        iniciar = findViewById(R.id.iniciar_sesion);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Iniciar sesion", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- CafeFront/app/src/main/java/com/example/cafefront/SeleccionActivity.java
+++ CafeFront/app/src/main/java/com/example/cafefront/SeleccionActivity.java
@@ -3,6 +3,7 @@ package com.example.cafefront;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,8 @@ public class SeleccionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Has clicado en trabajador", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(context, Login.class);
+                startActivity(intent);
             }
         });
 
