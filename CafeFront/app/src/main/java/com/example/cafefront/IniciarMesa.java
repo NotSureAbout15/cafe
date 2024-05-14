a769680b19b59fc59263197eec2f13889b06a180
NotSureAbout15 <tgcynthia04@gmail.com>
2024-05-14 22:03:59 +0200

    Añadidos spinners y buttons a la pantalla  y fijada su visibilidad

--- CafeFront/app/src/main/java/com/example/cafefront/IniciarMesa.java
+++ CafeFront/app/src/main/java/com/example/cafefront/IniciarMesa.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -25,7 +27,13 @@ import org.json.JSONObject;
 public class IniciarMesa extends AppCompatActivity {
     private Context context;
     private RequestQueue queue;
-    private Button harry;
+    private TextView zona_libros;
+    private TextView zona_deportes;
+    private TextView zona_musica;
+    private Spinner libros;
+    private Spinner deportes;
+    private Spinner musica;
+    private Button confirmar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +47,52 @@ public class IniciarMesa extends AppCompatActivity {
 
         context = this;
         queue = Volley.newRequestQueue(context);
-        harry = findViewById(R.id.harry_potter);
+        zona_libros = findViewById(R.id.zona_libros);
+        zona_deportes = findViewById(R.id.zona_deportes);
+        zona_musica = findViewById(R.id.zona_musica);
+        libros = findViewById(R.id.spinner_libros);
+        deportes = findViewById(R.id.spinner_deportes);
+        musica = findViewById(R.id.spinner_musica);
+        confirmar = findViewById(R.id.confirmar);
 
-        harry.setOnClickListener(new View.OnClickListener() {
+        //en un inicio hago q todos los spinners sean invisibles
+        libros.setVisibility(View.INVISIBLE);
+        deportes.setVisibility(View.INVISIBLE);
+        musica.setVisibility(View.INVISIBLE);
+
+        //al clicar sobre uno de los botones, sera visible su spinner y desapareceran el resto de elementos de la pantalla
+        zona_libros.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "Clicaste en: " + harry.getText().toString(), Toast.LENGTH_LONG).show();
+                libros.setVisibility(View.VISIBLE);
+                zona_deportes.setVisibility(View.INVISIBLE);
+                zona_musica.setVisibility(View.INVISIBLE);
+            }
+        });
+        zona_deportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deportes.setVisibility(View.VISIBLE);
+                zona_libros.setVisibility(View.INVISIBLE);
+                zona_musica.setVisibility(View.INVISIBLE);
+            }
+        });
+        zona_musica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musica.setVisibility(View.VISIBLE);
+                zona_libros.setVisibility(View.INVISIBLE);
+                zona_deportes.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        //al darle al boton confirmar envio la peticion para actualizar el estado de la mesa
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Clicaste en: " + confirmar.getText().toString(), Toast.LENGTH_LONG).show();
                 try {
-                    String mesa = harry.getText().toString();
+                    String mesa = confirmar.getText().toString();
                     mesaenuso(mesa);
                 }catch (Exception e){
                     System.out.println("Error: " + e);
