053541d6fa39075290a63bb731188f7c51e7d519
NotSureAbout15 <tgcynthia04@gmail.com>
2024-05-17 10:11:18 +0200

    Cambio en el models de Pedido para que solo guarde como objeto a la mesa y no al producto ni el precio

--- CafeAPI/CafeApp/models.py
+++ CafeAPI/CafeApp/models.py
@@ -23,7 +23,7 @@ class Menu(models.Model):
 
 
 class Pedido(models.Model):
-    pedido = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="comandas")
+    pedido = models.CharField(max_length=50, null=False)
     mesa = models.ForeignKey(Mesas, on_delete=models.CASCADE)
-    precio = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="monto")
+    precio = models.FloatField(null=False)
     cantidad = models.IntegerField(null=False)
