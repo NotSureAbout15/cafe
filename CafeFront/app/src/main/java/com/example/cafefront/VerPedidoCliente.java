53a79b31026de594d76a74a2bb7ec59e77d094d4
NotSureAbout15 <tgcynthia04@gmail.com>
2024-05-27 00:18:09 +0200

    Redirección a SeleccionActivity al confirmar pedido
    Este cambio advierte al cliente de que será redireccionado a la pantalla de selección y de que no podrá realizar otro pedido hasta que este haya sido pagado (ya que para liberar la mesa y poder realizar un pedido nuevo en la misma tiene que hacerlo el trabajador)

--- CafeFront/app/src/main/java/com/example/cafefront/VerPedidoCliente.java
+++ CafeFront/app/src/main/java/com/example/cafefront/VerPedidoCliente.java
@@ -1,7 +1,9 @@
 package com.example.cafefront;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -125,7 +127,29 @@ public class VerPedidoCliente extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(context, "Su pedido está siendo preparado", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(context, "Su pedido está siendo preparado", Toast.LENGTH_LONG).show();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                        builder.setTitle("Su pedido está siendo preparado");
+                        //uso una variable para el mensaje que se va a mostrar
+                        String message = "Se cerrará la aplicación y volverá a la pantalla inicial.\n\n" +
+                                "Recuerde que no puede realizar otro pedido hasta haber pagado por éste :)";
+                        builder.setMessage(message);
+
+                        builder.setPositiveButton("OK", (dialog, which) -> {
+                            // Acción cuando se pulsa OK
+                            dialog.dismiss();
+                            //esto iniciara la actividad SelccionActivity
+                            Intent intent = new Intent(context, SeleccionActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(intent);
+                            //esto finalizara la actividad actual
+                            if (context instanceof Activity) {
+                                ((Activity) context).finish();
+                            }
+                        });
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
                     }
                 },
                 new Response.ErrorListener() {
